bd614bc99cd282cb42c2025eb064618d34a83c93
Lakshya Bhandari <143229163+Zeroo-01@users.noreply.github.com>
2025-05-29 11:11:20 +0530

    Time: 63 ms (33.39%) | Memory: 61.8 MB (36.61%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool ispali(string s, int start, int end)
+    {
+        while(start<=end)
+        {
+            if(s[start]!=s[end])
+            {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+    void helper(int ind,string s,vector<vector<string>>& ans,vector<string>& ds)
+    {
+        if(ind==s.size())
+        {   
+            ans.push_back(ds);
+            return;
+        }
+        for(int i=ind;i<s.size();i++)
+        {
+            if(ispali(s,ind,i))
+            {
+                ds.push_back(s.substr(ind,i-ind+1));
+                helper(i+1,s,ans,ds);
+                ds.pop_back();
+            }
+            
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>ans;
+        vector<string>ds;
+        helper(0,s,ans,ds);
+        return ans;
+
+    }
+};
