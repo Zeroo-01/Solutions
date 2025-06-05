337badda6c5d0291624d9b31d95231ee12deda64
Lakshya Bhandari <143229163+Zeroo-01@users.noreply.github.com>
2025-06-06 03:19:40 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (32.24%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
-    int sum(vector<int>&ds)
-    {
-        int ans=0;
-        for(auto it:ds)
-        {
-            ans+=it;
-        }
-        return ans;
-    }
-    void helper(int k, int n,vector<vector<int>>&ans,vector<int>ds, int num)
-    {
-        if(k==0&&sum(ds)==n)
-        {
-            ans.push_back(ds);
+    void helper(int k, int n, vector<vector<int>>& ans, vector<int>& ds, int num, int sum) {
+        if (k == 0) {
+            if (sum == n)
+                ans.push_back(ds);
             return;
         }
-        if(num>9) return;
+        if (num > 9) return;
+
         ds.push_back(num);
-        helper(k-1,n,ans,ds,num+1);
+        helper(k - 1, n, ans, ds, num + 1, sum + num);
         ds.pop_back();
-        helper(k,n,ans,ds,num+1);
 
+        helper(k, n, ans, ds, num + 1, sum);
     }
-    vector<vector<int>> combinationSum3(int k, int n) {
-        
-        vector<vector<int>>ans;
-        vector<int>ds;
-        helper(k,n,ans,ds,1);
-        return ans;
-        
 
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        helper(k, n, ans, ds, 1, 0);
+        return ans;
     }
 };
