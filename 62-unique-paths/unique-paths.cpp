c6d5ddef6d72cc24b3ca632d958dd4376921b77a
Lakshya Bhandari <143229163+Zeroo-01@users.noreply.github.com>
2025-07-09 20:47:40 +0530

    Time: 3 ms (8.44%) | Memory: 9.4 MB (33.65%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int helper(int row,int col,vector<vector<int>>&dp)
+    {
+        if(row==0&&col==0)
+        {
+            return 1;
+        }
+        if(row<0||col<0)
+        {
+            return 0;
+        }
+        if(dp[row][col]!=-1)
+        {
+            return dp[row][col];
+        }
+        int left=helper(row,col-1,dp);
+        int right=helper(row-1,col,dp);
+        return dp[row][col]=left+right;
+
+    }
+    int uniquePaths(int m, int n) {
+       vector<vector<int>> dp(m, vector<int>(n, -1));
+       return helper(m-1,n-1,dp);
+
+    }
+};
