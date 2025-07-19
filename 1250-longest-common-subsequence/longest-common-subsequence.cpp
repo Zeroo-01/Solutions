fda3a65253ccdd9f24b9fa486bf2486b34f074f2
Lakshya Bhandari <143229163+Zeroo-01@users.noreply.github.com>
2025-07-20 01:17:50 +0530

    Time: 15 ms (91.30%) | Memory: 9.1 MB (92.45%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-int helper(int i1,int i2,string &text1, string &text2, vector<vector<int>>&dp)
-{
-    if(i1==text1.size()||i2==text2.size()) return 0;
-    if(dp[i1][i2]!=-1) return dp[i1][i2];
-    int pick=0;
-    if(text1[i1]==text2[i2])
-    {
-        pick=1+helper(i1+1,i2+1,text1,text2,dp);
-    }
-    int notpick=  max(helper(i1+1,i2,text1,text2,dp),helper(i1,i2+1,text1,text2,dp));
-    return dp[i1][i2]=max(pick,notpick);
+// int helper(int i1,int i2,string &text1, string &text2, vector<vector<int>>&dp)
+// {
+//     if(i1==text1.size()||i2==text2.size()) return 0;
+//     if(dp[i1][i2]!=-1) return dp[i1][i2];
+//     int pick=0;
+//     if(text1[i1]==text2[i2])
+//     {
+//         pick=1+helper(i1+1,i2+1,text1,text2,dp);
+//     }
+//     int notpick=  max(helper(i1+1,i2,text1,text2,dp),helper(i1,i2+1,text1,text2,dp));
+//     return dp[i1][i2]=max(pick,notpick);
 
 
-}
+// }
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        vector<int>prev(m+1,0),curr(m+1,0);
         for(int i=n-1;i>=0;i--)
         {
             for(int j=m-1;j>=0;j--)
             {
                 int pick=0;
-                if(text1[i]==text2[j]) pick=1+dp[i+1][j+1];
-                int notpick=max(dp[i+1][j],dp[i][j+1]);
-                dp[i][j]=max(pick,notpick);
+                if(text1[i]==text2[j]) pick=1+prev[j+1];
+                int notpick=max(prev[j],curr[j+1]);
+                curr[j]=max(pick,notpick);
             }
+            prev=curr;
         }
-        return dp[0][0];
+        return prev[0];
         
 
 
