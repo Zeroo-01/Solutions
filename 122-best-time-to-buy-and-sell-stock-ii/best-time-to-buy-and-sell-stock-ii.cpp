2700235130c2a6a8a268879dffad578dac70ed76
Lakshya Bhandari <143229163+Zeroo-01@users.noreply.github.com>
2025-07-22 02:04:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (50.54%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -22,19 +22,12 @@ public:
         vector<int>prev(2,0),curr(2,0);
         for(int i=n-1;i>=0;i--)
         {
-            for(int j=1;j>=0;j--)
-            {
-                if(j==1)
-                {
-                    curr[1]=max(prev[0]-prices[i],prev[1]);
-                }
-                else
-                {
-                   curr[0]= max(prev[0],prices[i]+prev[1]);
-                }
-            }
+            curr[1]=max(prev[0]-prices[i],prev[1]);   
+            curr[0]= max(prev[0],prices[i]+prev[1]);
             prev=curr;
         }
+            
+        
         return prev[1];
         //return helper(0,1,prices,dp);
     }
