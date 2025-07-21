00a9747381ddd114eb723e9554d4cc2e39928983
Lakshya Bhandari <143229163+Zeroo-01@users.noreply.github.com>
2025-07-22 04:37:35 +0530

    Time: 56 ms (71.42%) | Memory: 79.3 MB (85.05%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
-    int helper(int i,int buy,int t, vector<int>& prices,vector<vector<vector<int>>>&dp)
-    {
-        if(t==0) return 0;
-        if(i==prices.size()) return 0;
-        if(dp[i][buy][t]!=-1) return dp[i][buy][t];
-        int profit=0;
-        if(buy)
-        {
-            profit=max((-prices[i]+helper(i+1,0,t,prices,dp)),helper(i+1,1,t,prices,dp));
-        }
-        else
-        {
-            profit=max(prices[i]+helper(i+1,1,t-1,prices,dp),helper(i+1,0,t,prices,dp));
-        }
-        return dp[i][buy][t]=profit;
-    }
+    // int helper(int i,int buy,int t, vector<int>& prices,vector<vector<vector<int>>>&dp)
+    // {
+    //     if(t==0) return 0;
+    //     if(i==prices.size()) return 0;
+    //     if(dp[i][buy][t]!=-1) return dp[i][buy][t];
+    //     int profit=0;
+    //     if(buy)
+    //     {
+    //         profit=max((-prices[i]+helper(i+1,0,t,prices,dp)),helper(i+1,1,t,prices,dp));
+    //     }
+    //     else
+    //     {
+    //         profit=max(prices[i]+helper(i+1,1,t-1,prices,dp),helper(i+1,0,t,prices,dp));
+    //     }
+    //     return dp[i][buy][t]=profit;
+    // }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
-        return helper(0,1,2,prices,dp);
+        vector<vector<int>>after(2,vector<int>(3,0)),curr(2,vector<int>(3,0));
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<=1;j++)
+            {
+                for(int t=1;t<=2;t++)
+                {
+                    if(j)
+                        {
+                            curr[j][t]=max(-prices[i]+after[0][t],after[1][t]);
+                        }
+                        else
+                        {
+                            curr[j][t]=max(prices[i]+after[1][t-1],after[0][t]);
+                        }
+                }
+                
+            }
+            after=curr;
+        }
+        return after[1][2];
+        // return helper(0,1,2,prices,dp);
 
     }
 };
