a3978f4afb871830983db702ce27ca9ad290850e
Lakshya Bhandari <143229163+Zeroo-01@users.noreply.github.com>
2025-07-23 01:17:54 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (80.03%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
-// int helper(int i,int prev,vector<int>&nums,vector<vector<int>>&dp)
-// {
-//     if(i==nums.size()) return 0;
-//     if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-//     int nottake=helper(i+1,prev,nums,dp);
-//     int take=0;
-//     if(prev==-1||nums[i]>nums[prev])
-//     {
-//         take=1+helper(i+1,i,nums,dp);
-//     }
-//     return dp[i][prev+1]=max(take,nottake);
-// }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        // vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        // return helper(0,-1,nums,dp);
-        vector<int>dp(n,1);
-        int maxi=1;
-        for(int i=0;i<n;i++)
+        vector<int>temp;
+        temp.push_back(nums[0]);
+        for(int i=1;i<n;i++)
         {
-            for(int prev=0;prev<i;prev++)
+            if(nums[i]>temp.back())
             {
-                if(nums[prev]<nums[i])
-                {
-                    dp[i]=max(dp[i],1+dp[prev]);
-                    maxi=max(dp[i],maxi);
-                }
-                
+                temp.push_back(nums[i]);
+            }
+            else
+            {
+                int j=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[j]=nums[i];
             }
         }
-        return maxi;
-        
-    }
+        return temp.size();
+   }
 };
