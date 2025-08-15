6df17de5b9d307e05b365b7a061c2fe2ed447318
Lakshya Bhandari <143229163+Zeroo-01@users.noreply.github.com>
2025-08-15 19:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (52.41%) - LeetSync

---
+++ 342-power-of-four/power-of-four.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==0) return false;
+        while(n!=0)
+        {
+            if(n%4!=0)
+            {
+                if(n==1) return true;
+                return false;
+            }
+            n/=4;
+        }
+        return true;
+    }
+};
