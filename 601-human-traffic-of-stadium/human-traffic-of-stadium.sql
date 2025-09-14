2a60647f67302fa2700fdc23114aeacf3b96a330
Lakshya Bhandari <143229163+Zeroo-01@users.noreply.github.com>
2025-09-14 18:39:44 +0530

    Time: 342 ms (86.13%) | Memory: 0B (100.00%) - LeetSync

--- 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -1,10 +1,12 @@
-select id,visit_date,people from
-(select *,
+with cte as(select *,
 lead(people) over(order by visit_date) as nxt1,
 lead(people,2) over(order by visit_date) as nxt2,
 lag(people) over(order by visit_date) as prev1,
 lag(people,2)over(order by visit_date) as prev2
-from stadium)t
+from stadium)
+
+select id,visit_date,people from
+cte
 where people>=100 and ((nxt1>=100 and nxt2>=100) or(prev2>=100 and prev1>=100) or (nxt1>=100 and prev1>=100))
 order by visit_date
 
